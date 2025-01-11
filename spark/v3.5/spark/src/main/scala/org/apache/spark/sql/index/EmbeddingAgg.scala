acf6075c6efb1a28ac55eaf92ac9b4a3a28a29e2
Sumedh Sakdeo <sumedhsakdeo@gmail.com>
2025-01-11 07:43:35 -0800

    Working embedding computation

--- spark/v3.5/spark/src/main/scala/org/apache/spark/sql/index/EmbeddingAgg.scala
+++ spark/v3.5/spark/src/main/scala/org/apache/spark/sql/index/EmbeddingAgg.scala
@@ -21,73 +21,89 @@
 
 package org.apache.spark.sql.index
 
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import dev.langchain4j.data.document.Metadata
 import dev.langchain4j.data.embedding.Embedding
 import dev.langchain4j.data.segment.TextSegment
 import dev.langchain4j.model.embedding.EmbeddingModel
-import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel
+import dev.langchain4j.model.ollama.OllamaEmbeddingModel
+import dev.langchain4j.model.output.Response
+import org.apache.iceberg.spark.actions.{TextEmbedding, TextEmbeddingBuffer}
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.expressions.aggregate.{ImperativeAggregate, TypedImperativeAggregate}
 import org.apache.spark.sql.catalyst.trees.UnaryLike
+import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.types.{BinaryType, DataType}
+import org.apache.spark.unsafe.types.UTF8String
 
-import java.nio.charset.StandardCharsets
+import java.time.Duration
+import java.util.Collections
 
 case class EmbeddingAgg(
                          child: Expression,
                          mutableAggBufferOffset: Int = 0,
-                         inputAggBufferOffset: Int = 0) extends TypedImperativeAggregate[List[(TextSegment, Embedding)]]
+                         inputAggBufferOffset: Int = 0) extends TypedImperativeAggregate[TextEmbeddingBuffer]
   with UnaryLike[Expression] {
 
   override def nullable: Boolean = false
 
   override def dataType: DataType = BinaryType
 
-  override def createAggregationBuffer(): List[(TextSegment, Embedding)] = {
-    List.empty[(TextSegment, Embedding)]
+  def this(colName: String) = {
+    this(col(colName).expr, 0, 0)
   }
 
-  override def update(buffer: List[(TextSegment, Embedding)], input: InternalRow): List[(TextSegment, Embedding)] = {
+  override def createAggregationBuffer(): TextEmbeddingBuffer = {
+    val emptyList: java.util.List[TextEmbedding] = Collections.emptyList()
+    new TextEmbeddingBuffer(emptyList)
+  }
+
+  override def update(buffer: TextEmbeddingBuffer, input: InternalRow): TextEmbeddingBuffer = {
     val value = child.eval(input)
-    if (value != null && value.isInstanceOf[String]) {
-      val embeddingModel: EmbeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel()
+    if (value != null && value.isInstanceOf[UTF8String]) {
+
+      System.setProperty("ai.djl.offline", "true")
+      System.setProperty("DJL_OFFLINE", "true")
+
+      val embeddingModel = new AllMiniLmL6V2EmbeddingModel()
+      val response : Response[Embedding] = embeddingModel.embed(value.toString)
+      print(response.content())
+
+      val ollamaModel : EmbeddingModel = new OllamaEmbeddingModel(
+        "http://localhost:11434/",
+        "llama3.1",
+        Duration.ofSeconds(60),
+        3,
+        true,
+        true,
+        Collections.emptyMap()
+      )
+
       val metadata: Metadata = new Metadata()
       val textSegment: TextSegment = TextSegment.from(value.toString, metadata)
-      val embedding: Embedding = embeddingModel.embed(textSegment).content()
-      (textSegment, embedding) :: buffer
+      val embedding: Embedding = ollamaModel.embed(textSegment).content()
+      val singletonList: java.util.List[TextEmbedding] = Collections.singletonList(new TextEmbedding(textSegment, embedding))
+      buffer.merge(new TextEmbeddingBuffer(singletonList))
     } else {
-      null
+      val emptyList: java.util.List[TextEmbedding] = Collections.emptyList()
+      new TextEmbeddingBuffer(emptyList)
     }
   }
 
-  override def merge(buffer: List[(TextSegment, Embedding)], input: List[(TextSegment, Embedding)]):
-  List[(TextSegment, Embedding)] = buffer ::: input
+  override def merge(buffer: TextEmbeddingBuffer, input: TextEmbeddingBuffer):
+  TextEmbeddingBuffer = buffer.merge(input)
 
-  override def eval(buffer: List[(TextSegment, Embedding)]): Any = {
-    toBytes(buffer)
+  override def eval(buffer: TextEmbeddingBuffer): Any = {
+    buffer.serialize()
   }
 
-  private def toBytes(buffer: List[(TextSegment, Embedding)]) = {
-    val gson = new Gson()
-    gson.toJson(buffer).getBytes(StandardCharsets.UTF_8)
+  override def serialize(buffer: TextEmbeddingBuffer): Array[Byte] = {
+    buffer.serialize()
   }
 
-  override def serialize(buffer: List[(TextSegment, Embedding)]): Array[Byte] = {
-    toBytes(buffer)
-  }
-
-  override def deserialize(storageFormat: Array[Byte]): List[(TextSegment, Embedding)] = {
-    val jsonString = new String(storageFormat, StandardCharsets.UTF_8)
-    val listType = new TypeToken[List[(TextSegment, Embedding)]]() {}.getType
-    val gson = new Gson()
-    gson.fromJson(jsonString, listType)
-  }
-
-  override protected def withNewChildInternal(newChild: Expression): Expression = {
-    copy(child = newChild)
+  override def deserialize(storageFormat: Array[Byte]): TextEmbeddingBuffer = {
+    TextEmbeddingBuffer.deserialize(storageFormat)
   }
 
   override def withNewMutableAggBufferOffset(newMutableAggBufferOffset: Int): ImperativeAggregate = {
@@ -97,4 +113,12 @@ case class EmbeddingAgg(
   override def withNewInputAggBufferOffset(newInputAggBufferOffset: Int): ImperativeAggregate = {
     copy(inputAggBufferOffset = newInputAggBufferOffset)
   }
+
+  override protected def withNewChildInternal(newChild: Expression): Expression = {
+    copy(child = newChild)
+  }
+
+
 }
+
+
